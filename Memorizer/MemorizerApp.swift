9cc39408814bf48e901bad0c4662e278b10caa69
Yulia Gopachenko <yulia.gopachenko@gmail.com>
2022-11-10 13:49:38 +0400

    Initial Commit

---
+++ Memorizer/MemorizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizerApp.swift
+//  Memorizer
+//
+//  Created by Julia on 10.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
